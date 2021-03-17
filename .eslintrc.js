e074c3f11f9907378ac9b4675b1e71ace42b4b54
donskov <donskovdmitriyv@gmail.com>
2021-03-17 17:18:10 +0200

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0,
+  },
+};
