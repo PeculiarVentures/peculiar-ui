c1118aa79f9de0b1e13b1b8787003fbc1d2a82dd
donskov <donskovdmitriyv@gmail.com>
2021-03-23 13:32:48 +0200

    chore(color): change palette return type

--- packages/color/src/color.ts
+++ packages/color/src/color.ts
@@ -3,8 +3,19 @@ import { rgbToHsb } from './rgb_to_hsb';
 import { rgbToHex } from './rgb_to_hex';
 import { hsbToRgb } from './hsb_to_rgb';
 
-const saturationMinMax = [10, 30];
-const brightnessMinMax = [15, 100];
+type PaletteTypes = (
+  'tint-5' |
+  'tint-4' |
+  'tint-3' |
+  'tint-2' |
+  'tint-1' |
+  'base' |
+  'shade-1' |
+  'shade-2' |
+  'shade-3' |
+  'shade-4' |
+  'shade-5'
+);
 
 export class Color {
   private red: number;
@@ -13,15 +24,16 @@ export class Color {
 
   private blue: number;
 
-  public name: string;
+  private saturationMinMax = [10, 30];
 
-  constructor(color: string | [r: number, g: number, b: number], name: string = 'base') {
+  private brightnessMinMax = [15, 100];
+
+  constructor(color: string | [r: number, g: number, b: number]) {
     const [r, g, b] = typeof color === 'string' ? hexToRgb(color) : color;
 
     this.red = r;
     this.green = g;
     this.blue = b;
-    this.name = name;
   }
 
   toHex() {
@@ -40,13 +52,14 @@ export class Color {
     return rgbToHsb(this.red, this.green, this.blue);
   }
 
-  palette(steps = 5) {
+  palette(): Record<PaletteTypes, Color> {
     const [h, s, b] = this.toHsb();
-    const result: Color[] = [this];
-    let saturationTintStep = (s - saturationMinMax[0]) / steps;
-    let saturationShadeStep = (s - saturationMinMax[1]) / steps;
-    let brightnessTintStep = (brightnessMinMax[1] - b) / steps;
-    let brightnessShadeStep = (b - brightnessMinMax[0]) / steps;
+    const steps = 5;
+
+    let saturationTintStep = (s - this.saturationMinMax[0]) / steps;
+    let saturationShadeStep = (s - this.saturationMinMax[1]) / steps;
+    let brightnessTintStep = (this.brightnessMinMax[1] - b) / steps;
+    let brightnessShadeStep = (b - this.brightnessMinMax[0]) / steps;
 
     if (brightnessTintStep < 0) {
       brightnessTintStep = 0;
@@ -64,6 +77,9 @@ export class Color {
       saturationShadeStep = 0;
     }
 
+    const tints: Color[] = [];
+    const shades: Color[] = [];
+
     for (let i = 1; i <= steps; i += 1) {
       const tintRgb = hsbToRgb(
         h,
@@ -76,10 +92,22 @@ export class Color {
         Math.floor(b - brightnessShadeStep * i),
       );
 
-      result.unshift(new Color(tintRgb, `tint-${i}`));
-      result.push(new Color(shadeRgb, `shade-${i}`));
+      tints.push(new Color(tintRgb));
+      shades.push(new Color(shadeRgb));
     }
 
-    return result;
+    return {
+      'tint-5': tints[4],
+      'tint-4': tints[3],
+      'tint-3': tints[2],
+      'tint-2': tints[1],
+      'tint-1': tints[0],
+      base: this,
+      'shade-1': shades[0],
+      'shade-2': shades[1],
+      'shade-3': shades[2],
+      'shade-4': shades[3],
+      'shade-5': shades[4],
+    };
   }
 }
