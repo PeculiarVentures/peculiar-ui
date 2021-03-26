681ca6bb947db0d558fb79557202c293ca4dcf11
donskov <donskovdmitriyv@gmail.com>
2021-03-26 13:39:33 +0200

    chore(color): add support use hex without "#" to hexToRgb

--- packages/color/src/hex_to_rgb.ts
+++ packages/color/src/hex_to_rgb.ts
@@ -4,8 +4,9 @@
  *  hexToRgb('#fff') // =>
  */
 export function hexToRgb(hex: string): [r: number, g: number, b: number] {
-  const rgb = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (_, r, g, b) => `#${r}${r}${g}${g}${b}${b}`)
-    .substring(1)
+  const rgb = hex
+    .replace(/^#?/, '')
+    .replace(/([a-f\d])([a-f\d])([a-f\d])$/i, (_, r, g, b) => `${r}${r}${g}${g}${b}${b}`)
     .match(/.{2}/g)
     .map((value) => parseInt(value, 16));
 
