6ded4e7d981e7e1de23591b7955b29a995b133be
donskov <donskovdmitriyv@gmail.com>
2021-04-01 18:38:31 +0300

    feat(react-components): add "noWrap" prop to Typography component

--- packages/react-components/src/Typography/typography.tsx
+++ packages/react-components/src/Typography/typography.tsx
@@ -13,6 +13,12 @@ type BaseProps = {
   className?: string;
   as?: React.ElementType,
   dataTestId?: string;
+  /**
+   * If `true`, the text will not wrap, but instead will truncate with a text overflow ellipsis.
+   * Note that text overflow can only happen with block or inline-block level elements
+   * (the element needs to have a width in order to overflow).
+   */
+  noWrap?: boolean;
 };
 
 type TypographyProps = BaseProps & React.HTMLAttributes<HTMLElement>;
@@ -22,6 +28,26 @@ const stylesBase = () => css({
   margin: 0,
 });
 
+const stylesVariant = (variant: TypographyType) => css({
+  label: variant,
+  fontWeight: `var(--pv-text-${variant}-weight)`,
+  fontSize: `var(--pv-text-${variant}-size)`,
+  lineHeight: `var(--pv-text-${variant}-height)`,
+  letterSpacing: `var(--pv-text-${variant}-spacing)`,
+} as any);
+
+const stylesColor = (color: ColorType) => css({
+  label: color,
+  color: `var(--pv-color-${color})`,
+});
+
+const stylesNoWrap = () => css({
+  label: 'noWrap',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+  textOverflow: 'ellipsis',
+});
+
 export const Typography = React.forwardRef<HTMLElement, TypographyProps>((props, ref) => {
   const {
     variant,
@@ -29,6 +55,7 @@ export const Typography = React.forwardRef<HTMLElement, TypographyProps>((props,
     as,
     color,
     dataTestId,
+    noWrap,
     ...other
   } = props;
   let asComponent = 'p';
@@ -42,24 +69,16 @@ export const Typography = React.forwardRef<HTMLElement, TypographyProps>((props,
   }
 
   return React.createElement(as || asComponent, {
-    ref,
-    className: cx(
-      stylesBase(),
-      css({
-        label: variant,
-        fontWeight: `var(--pv-text-${variant}-weight)`,
-        fontSize: `var(--pv-text-${variant}-size)`,
-        lineHeight: `var(--pv-text-${variant}-height)`,
-        letterSpacing: `var(--pv-text-${variant}-spacing)`,
-      } as any),
-      (color && css({
-        label: color,
-        color: `var(--pv-color-${color})`,
-      })),
-      className,
-    ),
-    'data-testid': dataTestId,
     ...other,
+    ref,
+    className: cx({
+      [stylesBase()]: true,
+      [stylesVariant(variant)]: !!variant,
+      [stylesColor(color)]: !!color,
+      [stylesNoWrap()]: noWrap,
+      [className]: !!className,
+    }),
+    'data-testid': dataTestId,
   });
 });
 
