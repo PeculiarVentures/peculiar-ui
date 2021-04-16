a2ccdb9a9eab6266da1e6db5a9e4947168bfd2ba
donskov <donskovdmitriyv@gmail.com>
2021-04-16 17:17:13 +0300

    feat(react-components): move FocusTrap to separate component

---
+++ packages/react-components/src/FocusTrap/focus_trap.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import FocusTrapReact from 'focus-trap-react';
+
+type BaseProps = {
+  /**
+   * A single child content element.
+   */
+  children: React.ReactElement;
+  /**
+   * If true, focus will be locked.
+   */
+  open: boolean;
+  /**
+   * If `true`, the trap focus will not automatically
+   * shift focus to itself when it opens.
+   */
+  // TODO: Need to implement.
+  disableAutoFocus?: boolean;
+};
+
+// TODO: Add handler for error from `FocusTrapReact` for container without interactive elements.
+export const FocusTrap: React.FC<BaseProps> = (props) => {
+  const {
+    children,
+    open,
+  } = props;
+
+  return (
+    <FocusTrapReact
+      active={open}
+    >
+      {children}
+    </FocusTrapReact>
+  );
+};
+
+FocusTrap.displayName = 'FocusTrap';
+
+FocusTrap.defaultProps = {};
