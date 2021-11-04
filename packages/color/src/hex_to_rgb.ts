6694700ff0f9de07474f1f0c8849011a1409f988
mariktar <100trk@gmail.com>
2021-11-04 18:19:38 +0200

    chore(color): added short hex format supporting

--- packages/color/src/hex_to_rgb.ts
+++ packages/color/src/hex_to_rgb.ts
@@ -1,13 +1,17 @@
 /**
  * HEX color to RGB color.
  * @example
- *  hexToRgb('#FFFFFF') // =>
+ *  hexToRgb('#FFCCFF') // => [255,204,255]
+ *  or
+ *  hexToRgb('#FCF') // => [255,204,255]
  */
 export function hexToRgb(hex: string): [r: number, g: number, b: number] {
-  const rgb = hex
-    .replace(/^#?/, '')
-    .match(/.{2}/g)
-    .map((value) => parseInt(value, 16));
+  const clearHex = hex.replace(/^#?/, '');
+  const isShortHex = clearHex.length === 3;
 
-  return [rgb[0], rgb[1], rgb[2]];
+  const [r, g, b] = isShortHex
+    ? clearHex.match(/.{1}/g).map((value) => parseInt(value + value, 16))
+    : clearHex.match(/.{2}/g).map((value) => parseInt(value, 16));
+
+  return [r, g, b];
 }
