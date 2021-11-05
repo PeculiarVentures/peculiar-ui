fe61bef7aae7641901806701b3300c305ba89171
Taras <47363177+MarikTar@users.noreply.github.com>
2021-11-05 11:48:59 +0200

    Color: Add support short hex format (#36)
    * chore(color): added short hex format supporting
    
    * chore(color): update hexToRgb short hex handling
    
    Co-authored-by: donskov <donskovdmitriyv@gmail.com>

--- packages/color/src/hex_to_rgb.test.ts
+++ packages/color/src/hex_to_rgb.test.ts
@@ -4,4 +4,8 @@ describe('hexToRgb', () => {
   it('converts a long hex color to a rgb color` ', () => {
     expect(hexToRgb('#a94fd3')).toEqual([169, 79, 211]);
   });
+
+  it('converts a short hex color to a rgb color` ', () => {
+    expect(hexToRgb('#fcf')).toEqual([255, 204, 255]);
+  });
 });
--- packages/color/src/hex_to_rgb.ts
+++ packages/color/src/hex_to_rgb.ts
@@ -1,10 +1,13 @@
 /**
  * HEX color to RGB color.
  * @example
- *  hexToRgb('#FFFFFF') // =>
+ *  hexToRgb('#FFCCFF') // => [255,204,255]
  */
 export function hexToRgb(hex: string): [r: number, g: number, b: number] {
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+
   const rgb = hex
+    .replace(shorthandRegex, (_, r, g, b) => r + r + g + g + b + b)
     .replace(/^#?/, '')
     .match(/.{2}/g)
     .map((value) => parseInt(value, 16));
