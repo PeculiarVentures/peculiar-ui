40b4ad2fdb515db04a823e64f65550ed9ca79f8e
donskov <donskovdmitriyv@gmail.com>
2021-11-05 11:48:15 +0200

    chore(color): update hexToRgb short hex handling

--- packages/color/src/hex_to_rgb.ts
+++ packages/color/src/hex_to_rgb.ts
@@ -2,16 +2,15 @@
  * HEX color to RGB color.
  * @example
  *  hexToRgb('#FFCCFF') // => [255,204,255]
- *  or
- *  hexToRgb('#FCF') // => [255,204,255]
  */
 export function hexToRgb(hex: string): [r: number, g: number, b: number] {
-  const clearHex = hex.replace(/^#?/, '');
-  const isShortHex = clearHex.length === 3;
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
 
-  const [r, g, b] = isShortHex
-    ? clearHex.match(/.{1}/g).map((value) => parseInt(value + value, 16))
-    : clearHex.match(/.{2}/g).map((value) => parseInt(value, 16));
+  const rgb = hex
+    .replace(shorthandRegex, (_, r, g, b) => r + r + g + g + b + b)
+    .replace(/^#?/, '')
+    .match(/.{2}/g)
+    .map((value) => parseInt(value, 16));
 
-  return [r, g, b];
+  return [rgb[0], rgb[1], rgb[2]];
 }
