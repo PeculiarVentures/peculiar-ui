be40964568d57cc0e40beca18b8d539b91076c78
mariktar <100trk@gmail.com>
2021-12-17 14:10:01 +0200

    chore(react-component): remove pinned prop

--- packages/react-components/src/Dialog/dialog_content.tsx
+++ packages/react-components/src/Dialog/dialog_content.tsx
@@ -21,10 +21,6 @@ type BaseProps = {
    * The content of the error message.
    */
   error?: string;
-  /**
-   * The content of pinned part.
-   */
-  pinnedContent?: React.ReactNode,
 };
 
 type DialogContentProps = BaseProps & React.HTMLAttributes<HTMLDivElement>;
@@ -35,7 +31,6 @@ const stylesBase = () => css({
   overflow: 'hidden',
   display: 'flex',
   flexDirection: 'column',
-  padding: 'var(--pv-size-base-3) 0 0',
 });
 
 const stylesPinned = () => css({
@@ -46,18 +41,19 @@ const stylesPinned = () => css({
 
 const stylesScrolled = () => css({
   label: 'DialogScrolledContent',
-  padding: '0 var(--pv-size-base-4) var(--pv-size-base-3)',
   overflowY: 'auto',
   flex: '1 1 auto',
+  padding: 'var(--pv-size-base-3) var(--pv-size-base-4)',
 });
 
 const stylesError = () => css({
   label: 'DialogContent-error',
+  margin: 'var(--pv-size-base-2) 0',
   padding: 'var(--pv-size-base-2) var(--pv-size-base-3)',
 });
 
 const stylesCollapse = () => css({
-  margin: 'calc(var(--pv-size-base-2) * -1) calc(var(--pv-size-base-3) * -1) var(--pv-size-base-2)',
+  margin: '0 calc(var(--pv-size-base-3) * -1)',
 });
 
 export const DialogContent = React.forwardRef<HTMLDivElement, DialogContentProps>((props, ref) => {
@@ -66,7 +62,6 @@ export const DialogContent = React.forwardRef<HTMLDivElement, DialogContentProps
     className,
     dividers,
     error,
-    pinnedContent,
     ...other
   } = props;
 
@@ -97,7 +92,6 @@ export const DialogContent = React.forwardRef<HTMLDivElement, DialogContentProps
             {error}
           </Alert>
         </Collapse>
-        {pinnedContent}
       </div>
       <div className={cx(stylesScrolled())}>
         {children}
