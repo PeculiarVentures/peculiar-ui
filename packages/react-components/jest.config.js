78beb9a878995965129743759b97a3368a0f6982
Illia Kharlamov <47363167+Kharya1337@users.noreply.github.com>
2023-03-02 09:24:41 +0000

    chore(react_components): fix tests fail

--- packages/react-components/jest.config.js
+++ packages/react-components/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
+    "@testing-library/jest-dom/extend-expect",
+    "<rootDir>/src/setupTests.js"
   ],
   watchPlugins: [
     "jest-watch-typeahead/filename",
