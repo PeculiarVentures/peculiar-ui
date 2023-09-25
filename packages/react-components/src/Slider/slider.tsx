6517ef41c614bd89e3d144ee704c45467bf4fd31
mariktar <100trk@gmail.com>
2023-09-25 14:56:06 +0300

    feat(react-components): add "styled" to Slider

--- packages/react-components/src/Slider/slider.tsx
+++ packages/react-components/src/Slider/slider.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { css, cx } from '../styles';
+import styled from '@emotion/styled';
 
 type BaseProps = {
   className?: string;
@@ -40,14 +40,15 @@ type BaseProps = {
 
 export type SliderProps = BaseProps & Omit<React.HTMLAttributes<HTMLDivElement>, 'children' | 'defaultValue'>;
 
-const stylesBase = () => css({
-  label: 'Slider',
+/**
+ * Styles.
+ */
+const SliderRoot = styled('div')<SliderProps>({
   height: '16px',
   width: '100%',
 });
 
-const stylesInput = () => css({
-  label: 'Slider-input',
+const InputRoot = styled('input')({
   appearance: 'none',
   width: '100%',
   height: '100%',
@@ -99,6 +100,9 @@ const stylesInput = () => css({
     },
   },
 });
+/**
+ *
+ */
 
 export const Slider = React.forwardRef<HTMLDivElement, SliderProps>((props, ref) => {
   const {
@@ -115,27 +119,22 @@ export const Slider = React.forwardRef<HTMLDivElement, SliderProps>((props, ref)
   } = props;
 
   return (
-    <div
+    <SliderRoot
       {...other}
       ref={ref}
-      className={cx({
-        [stylesBase()]: true,
-        [className]: !!className,
-      })}
     >
-      <input
+      <InputRoot
         type="range"
         name={name}
         min={min}
         max={max}
         step={step}
-        className={cx(stylesInput())}
         value={value}
         defaultValue={defaultValue}
         onChange={onChange}
         disabled={disabled}
       />
-    </div>
+    </SliderRoot>
   );
 });
 
