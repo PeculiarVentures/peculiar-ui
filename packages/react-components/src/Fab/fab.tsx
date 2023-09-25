bd9a8350a37e182552a3e68bb797b2a624c8223c
mariktar <100trk@gmail.com>
2023-09-25 11:59:32 +0300

    feat(react-components): add "styled" to fab

--- packages/react-components/src/Fab/fab.tsx
+++ packages/react-components/src/Fab/fab.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import styled from '@emotion/styled';
 import { OverridableComponent, OverrideProps } from '../OverridableComponent';
-import { css, cx } from '../styles';
-import { ButtonBase, ButtonBaseOwnProps } from '../ButtonBase';
+import { ButtonBaseOwnProps } from '../ButtonBase';
+import { Button } from '../Button';
 
 /**
  * Types.
@@ -33,38 +34,23 @@ export type FabProps<
 /**
  * Styles.
  */
-const stylesBase = () => css({
-  label: 'Fab',
+const FabRoot = styled(Button)<FabOwnProps>(() => ({
   borderRadius: '50%',
   height: 'var(--pv-size-base-11)',
   width: 'var(--pv-size-base-11)',
   padding: 'var(--pv-size-base-2)',
-});
+}));
 /**
  *
  */
 
-export const Fab = React.forwardRef<any, FabProps>((props, ref) => {
-  const {
-    className,
-    children,
-    ...other
-  } = props;
-
-  return (
-    <ButtonBase
-      {...other}
-      ref={ref}
-      className={cx({
-        [stylesBase()]: true,
-        [className]: !!className,
-      })}
-      size="large"
-    >
-      {children}
-    </ButtonBase>
-  );
-}) as OverridableComponent<FabTypeMap>;
+export const Fab = React.forwardRef<any, FabProps>((props, ref) => (
+  <FabRoot
+    ref={ref}
+    size="large"
+    {...props}
+  />
+)) as OverridableComponent<FabTypeMap>;
 
 Fab.displayName = 'Fab';
 
