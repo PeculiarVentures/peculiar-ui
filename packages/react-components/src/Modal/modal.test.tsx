f6bb3881925e67d27572b542e5317e4d4da0a65e
donskov <donskovdmitriyv@gmail.com>
2023-09-26 18:51:08 +0300

    tests(react-components): add Modal tests

--- packages/react-components/src/Modal/modal.test.tsx
+++ packages/react-components/src/Modal/modal.test.tsx
@@ -4,12 +4,32 @@ import { Modal } from './index';
 
 describe('<Modal />', () => {
   it('should render with default styles', () => {
-    const { asFragment } = render(
+    const { baseElement } = render(
+      <Modal open={false}>
+        <div>Inside</div>
+      </Modal>,
+    );
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should render with default open styles', () => {
+    const { baseElement } = render(
       <Modal open>
         <div>Inside</div>
       </Modal>,
     );
 
-    expect(asFragment()).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('should pass className', () => {
+    const { baseElement } = render(
+      <Modal open className="my-class-name">
+        <div>Inside</div>
+      </Modal>,
+    );
+
+    expect(baseElement).toMatchSnapshot();
   });
 });
