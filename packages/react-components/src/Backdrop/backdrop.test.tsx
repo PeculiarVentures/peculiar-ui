3e93e367cac4954e5561333154084ac7a3d97497
donskov <donskovdmitriyv@gmail.com>
2023-09-26 17:20:15 +0300

    tests(react-components): add Backdrop tests

--- packages/react-components/src/Backdrop/backdrop.test.tsx
+++ packages/react-components/src/Backdrop/backdrop.test.tsx
@@ -3,13 +3,37 @@ import { renderWithWrapper as render } from '../test-utils';
 import { Backdrop } from './index';
 
 describe('<Backdrop />', () => {
-  describe('Backdrop render variants', () => {
-    it('should render as default', () => {
-      const { asFragment } = render(
-        <Backdrop open />,
-      );
+  it('should render with default styles', () => {
+    const { asFragment } = render(
+      <Backdrop open />,
+    );
 
-      expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should pass className', () => {
+    const { asFragment } = render(
+      <Backdrop open className="my-class-name" />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('variants', () => {
+    const variants: Array<React.ComponentProps<typeof Backdrop>['variant']> = [
+      'light',
+      'medium',
+      'heavy',
+    ];
+
+    variants.forEach((variant) => {
+      it(`variant "${variant}"`, () => {
+        const { asFragment } = render(
+          <Backdrop open variant={variant} />,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
     });
   });
 });
