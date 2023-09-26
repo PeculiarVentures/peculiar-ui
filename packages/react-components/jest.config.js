f8722f9990ef3be9a53c8c5802306ff6a2d86f87
donskov <donskovdmitriyv@gmail.com>
2023-09-26 14:07:33 +0300

    tests(react-components): connect snapshots

--- packages/react-components/jest.config.js
+++ packages/react-components/jest.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect",
-    "<rootDir>/src/setupTests.js"
+    '@testing-library/jest-dom/extend-expect',
+    '<rootDir>/src/setupTests.js'
   ],
   watchPlugins: [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname",
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
   ],
   transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
-  "transformIgnorePatterns": [
-    "node_modules/(?!nanoid/.*)"
+  transformIgnorePatterns: [
+    'node_modules/(?!nanoid/.*)'
+  ],
+  snapshotSerializers: [
+    '@emotion/jest/serializer',
   ],
 };
