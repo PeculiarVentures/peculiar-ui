b2af7cb1c84cb26be002746b0cc8118149c779ae
donskov <donskovdmitriyv@gmail.com>
2023-09-26 17:07:02 +0300

    tests(react-components): add Alert tests

--- packages/react-components/src/Alert/alert.test.tsx
+++ packages/react-components/src/Alert/alert.test.tsx
@@ -12,4 +12,49 @@ describe('<Alert />', () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('should pass className', () => {
+    const { asFragment } = render(
+      <Alert className="my-class-name">
+        Alert message
+      </Alert>,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('variants', () => {
+    const variants: Array<React.ComponentProps<typeof Alert>['variant']> = [
+      'attention',
+      'pending',
+      'success',
+      'wrong',
+    ];
+
+    variants.forEach((variant) => {
+      describe(`variant "${variant}"`, () => {
+        it('enable icon', () => {
+          const { asFragment } = render(
+            <Alert variant={variant} disableIcon={false}>
+              Message
+              {variant}
+            </Alert>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        });
+
+        it('disable icon', () => {
+          const { asFragment } = render(
+            <Alert variant={variant} disableIcon>
+              Message
+              {variant}
+            </Alert>,
+          );
+
+          expect(asFragment()).toMatchSnapshot();
+        });
+      });
+    });
+  });
 });
