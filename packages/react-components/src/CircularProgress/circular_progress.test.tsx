627462b3dd29f3800562d42f4a716f56a87a2d6a
donskov <donskovdmitriyv@gmail.com>
2023-09-26 17:40:11 +0300

    tests(react-components): add CircularProgress tests

--- packages/react-components/src/CircularProgress/circular_progress.test.tsx
+++ packages/react-components/src/CircularProgress/circular_progress.test.tsx
@@ -8,4 +8,45 @@ describe('<CircularProgress />', () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('should pass className', () => {
+    const { asFragment } = render(<CircularProgress className="my-class-name" />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('sizes', () => {
+    const sizes: Array<React.ComponentProps<typeof CircularProgress>['size']> = [
+      'small',
+      'large',
+    ];
+
+    sizes.forEach((size) => {
+      it(`size "${size}"`, () => {
+        const { asFragment } = render(
+          <CircularProgress size={size} />,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('colors', () => {
+    const colors: Array<React.ComponentProps<typeof CircularProgress>['color']> = [
+      'primary',
+      'secondary',
+      'white',
+    ];
+
+    colors.forEach((color) => {
+      it(`color "${color}"`, () => {
+        const { asFragment } = render(
+          <CircularProgress color={color} />,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
+    });
+  });
 });
