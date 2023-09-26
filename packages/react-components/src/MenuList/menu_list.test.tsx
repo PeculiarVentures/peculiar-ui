b8a7637070f59edda5cc92dae8264b0cb6d38529
mariktar <100trk@gmail.com>
2023-09-26 19:05:50 +0300

    tests(react-components): add MenuItem, MenuList tests

--- packages/react-components/src/MenuList/menu_list.test.tsx
+++ packages/react-components/src/MenuList/menu_list.test.tsx
@@ -14,4 +14,16 @@ describe('<MenuList />', () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('should pass className', () => {
+    const { asFragment } = render(
+      <MenuList className="my-class-name">
+        <li>Item-1</li>
+        <li>Item-2</li>
+        <li>Item-3</li>
+      </MenuList>,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
