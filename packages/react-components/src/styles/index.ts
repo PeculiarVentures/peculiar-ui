fd580e1957190d4ad4e9f0feefa9046c500642d5
donskov <donskovdmitriyv@gmail.com>
2023-09-26 13:09:09 +0300

    chore(react-components): remove css export

--- packages/react-components/src/styles/index.ts
+++ packages/react-components/src/styles/index.ts
@@ -1,9 +1,3 @@
 export { ThemeProvider } from './theme_provider';
-export {
-  css,
-  cx,
-  // keyframes,
-  // cache,
-} from './css';
 export * from './types';
 export { defaultTheme } from './default_theme';
