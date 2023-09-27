d5dd25b346a5b94df18c81f1091ea5e079db2dab
donskov <donskovdmitriyv@gmail.com>
2023-09-27 13:07:15 +0300

    tests(react-components): add Autocomplete tests

--- packages/react-components/src/Autocomplete/autocomplete.test.tsx
+++ packages/react-components/src/Autocomplete/autocomplete.test.tsx
@@ -3,18 +3,62 @@ import { renderWithWrapper as render } from '../test-utils';
 import { Autocomplete } from './index';
 
 describe('<Autocomplete />', () => {
-  describe('Autocomplete render variants', () => {
-    it('should render as default', () => {
-      const options = ['test-1', 'test-2'];
+  const options = ['test-1', 'test-2'];
 
-      const { asFragment } = render(
-        <Autocomplete
-          options={options}
-          id="test-id"
-        />,
-      );
+  it('should render with default styles', () => {
+    const { asFragment } = render(
+      <Autocomplete
+        options={options}
+        id="test-id"
+      />,
+    );
 
-      expect(asFragment()).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render with default multiple styles', () => {
+    const { asFragment } = render(
+      <Autocomplete
+        options={options}
+        id="test-id"
+        multiple
+      />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should pass className', () => {
+    const { asFragment } = render(
+      <Autocomplete
+        options={options}
+        id="test-id"
+        className="my-class-name"
+      />,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('sizes', () => {
+    const sizes: Array<React.ComponentProps<typeof Autocomplete>['size']> = [
+      'small',
+      'medium',
+      'large',
+    ];
+
+    sizes.forEach((size) => {
+      it(`size "${size}"`, () => {
+        const { asFragment } = render(
+          <Autocomplete
+            options={options}
+            id="test-id"
+            size={size}
+          />,
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+      });
     });
   });
 });
