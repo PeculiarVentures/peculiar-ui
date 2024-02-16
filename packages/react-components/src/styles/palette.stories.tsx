ce9ba245f1c7842f448210ed19d655dc63508eb3
donskov <donskovdmitriyv@gmail.com>
2024-02-16 15:34:45 +0200

    docs: add PalettePlayground section

--- packages/react-components/src/styles/palette.stories.tsx
+++ packages/react-components/src/styles/palette.stories.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Color } from '@peculiar/color';
 import { Typography } from '../Typography';
+import { colors as colorsTheme } from './foundations';
+import { contrastThreshold } from './utils';
+import { defaultThemeLight } from './default_theme';
 
 export default {
   title: 'Palette',
@@ -42,5 +46,44 @@ export const ColorItemExample: React.FC<{ title: string; colors: Record<string, 
         ))}
       </ul>
     </div>
+  );
+};
+
+export const PalettePlayground = () => {
+  const [color, setColor] = React.useState<string>(colorsTheme.primary.primary);
+  const palette = colorsTheme.generateColorPalette('custom', color);
+
+  const contrastColorRatio = new Color(color)
+    .getContrastRatio(defaultThemeLight.color.white);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="color_item_custom-picker">
+        <input
+          type="color"
+          defaultValue={color}
+          onChange={handleChange}
+          className="color_item_custom-picker_input"
+        />
+        <h4
+          style={{
+            color: contrastColorRatio > contrastThreshold
+              ? defaultThemeLight.color.white
+              : defaultThemeLight.color.black,
+          }}
+          className="color_item_custom-picker_text"
+        >
+          {color}
+        </h4>
+      </div>
+      <ColorItemExample
+        title="Result"
+        colors={palette}
+      />
+    </>
   );
 };
