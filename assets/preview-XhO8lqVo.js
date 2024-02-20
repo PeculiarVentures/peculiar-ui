af41259f9af38283d85aeff084c65af6caa9e1f3
donskov <donskov@users.noreply.github.com>
2024-02-20 15:44:47 +0000

    deploy: 655fd2fc54e0855492308b23306b236059ee8459

---
+++ assets/preview-XhO8lqVo.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-j_Qx72rA.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BOjHdw2a.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BOjHdw2a.js","./chunk-HLWAVYOI-UQ43M7rw.js","./iframe-j_Qx72rA.js","./index-OjgoNOWw.js","./react-18-DyM-o0Ps.js","./index-mQqIOHEI.js","./index-A854vZC-.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js","./inheritsLoose-Y9jOMJLd.js","./index-LnHysSno.js","./index-BdDYuicC.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
