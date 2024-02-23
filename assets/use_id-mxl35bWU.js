c28933d77904f538d9c968eba197420f32c1a6c0
donskov <donskov@users.noreply.github.com>
2024-02-23 16:49:14 +0000

    deploy: c30bd2ebd163c1d614aead8ed6cc5a6223b2851e

---
+++ assets/use_id-mxl35bWU.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{M as i,C as r}from"./index-hvC27ksF.js";import{DemoExample as c}from"./use_id.stories-dkxlW0IT.js";import{useMDXComponents as s}from"./index-82O-B91n.js";import"./index-OjgoNOWw.js";import"./iframe-xyW4GGK5.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js";import"./index-mQqIOHEI.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-LnHysSno.js";import"./index-BdDYuicC.js";import"./use_id-vDNgflVU.js";function o(t){const n=Object.assign({h2:"h2",p:"p",pre:"pre",code:"code"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Hooks/useId"}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx(n.p,{children:"React hook for generating unique IDs that can be passed to accessibility attributes."}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { useId } from '@peculiar/react-components';
+
+const Demo = () => {
+  const id = useId();
+
+  return (
+    <div>
+      ID is:
+      {' '}
+      {id}
+    </div>
+  );
+};
+`})}),`
+`,e.jsx(n.h2,{id:"demo",children:"Demo"}),`
+`,e.jsx(r,{of:c})]})}function b(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{b as default};
