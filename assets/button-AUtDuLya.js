276d71a63f3aa69cb359d8d63ca13b78848b84d7
donskov <donskov@users.noreply.github.com>
2024-02-23 16:48:51 +0000

    deploy: ba16131780d688ce9a46965c9fbddcf03c55696b

---
+++ assets/button-AUtDuLya.js
@@ -0,0 +1,31 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{M as s,C as n,A as c}from"./index-RoO4N3Pl.js";import{S as a,T as l,C as d,O as h,a as x,D as m,P as j,b as p,W as u,c as f,I as b}from"./button.stories-xJm-TVmW.js";import{B as g}from"./button-gyaUpdMj.js";import{useMDXComponents as i}from"./index-82O-B91n.js";import"./index-OjgoNOWw.js";import"./iframe-b4rHzHn3.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js";import"./index-mQqIOHEI.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-LnHysSno.js";import"./index-BdDYuicC.js";import"./plus_icon-u0qdKX2a.js";import"./close_icon-Bp311Jgy.js";import"./emotion-styled.browser.esm-jsqElTi7.js";import"./emotion-element-c39617d8.browser.esm-upVMHClS.js";import"./button_base-v55xmT6U.js";import"./typography-mH1J_rqf.js";function r(t){const e=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",h4:"h4"},i(),t.components);return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:a}),`
+`,o.jsx(e.h1,{id:"button",children:"Button"}),`
+`,o.jsx(e.p,{children:"Buttons allow users to take actions, and make choices, with a single tap."}),`
+`,o.jsx(e.h2,{id:"variant",children:"Variant"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"Button"})," comes with three variants: ",o.jsx(e.code,{children:"text"})," (default), ",o.jsx(e.code,{children:"contained"}),", and ",o.jsx(e.code,{children:"outlined"}),"."]}),`
+`,o.jsx(e.h4,{id:"text-button",children:"Text button"}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(e.h4,{id:"contained-button",children:"Contained button"}),`
+`,o.jsx(n,{of:d}),`
+`,o.jsx(e.h4,{id:"outlined-button",children:"Outlined button"}),`
+`,o.jsx(n,{of:h}),`
+`,o.jsx(e.h2,{id:"size",children:"Size"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"size"})," prop to change the size of the button. You can set the value to ",o.jsx(e.code,{children:"small"}),", ",o.jsx(e.code,{children:"medium"})," (default) or ",o.jsx(e.code,{children:"large"}),"."]}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(e.h2,{id:"color",children:"Color"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"color"})," prop to change the color of the button. You can set the value to ",o.jsx(e.code,{children:"default"})," (default), ",o.jsx(e.code,{children:"primary"}),", ",o.jsx(e.code,{children:"secondary"}),", ",o.jsx(e.code,{children:"wrong"})," or ",o.jsx(e.code,{children:"white"}),"."]}),`
+`,o.jsx(e.h4,{id:"default-color",children:"Default color"}),`
+`,o.jsx(n,{of:m}),`
+`,o.jsx(e.h4,{id:"primary-color",children:"Primary color"}),`
+`,o.jsx(n,{of:j}),`
+`,o.jsx(e.h4,{id:"secondary-color",children:"Secondary color"}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(e.h4,{id:"wrong-color",children:"Wrong color"}),`
+`,o.jsx(n,{of:u}),`
+`,o.jsx(e.h4,{id:"white-color",children:"White color"}),`
+`,o.jsx(n,{of:f,className:"bg-dark"}),`
+`,o.jsx(e.h2,{id:"icon-button",children:"Icon button"}),`
+`,o.jsx(e.p,{children:"Sometimes you might want to have icons for certain buttons to enhance the UX of the application as we recognize logos more easily than plain text."}),`
+`,o.jsx(n,{of:b}),`
+`,o.jsx(e.h2,{id:"component-api",children:"Component API"}),`
+`,o.jsx(c,{of:g})]})}function Y(t={}){const{wrapper:e}=Object.assign({},i(),t.components);return e?o.jsx(e,Object.assign({},t,{children:o.jsx(r,t)})):r(t)}export{Y as default};
