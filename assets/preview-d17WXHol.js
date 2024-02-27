6c9da04cddb72fe993edd2856146c887cbb5733c
donskov <donskov@users.noreply.github.com>
2024-02-27 16:54:52 +0000

    deploy: d6d8eb8f7c102b74d05e5f5800521255bdf50bfc

---
+++ assets/preview-d17WXHol.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-sYsqnhir.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-YOtBG8aK.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-YOtBG8aK.js","./chunk-HLWAVYOI-5GeYPCLN.js","./iframe-sYsqnhir.js","./index-OjgoNOWw.js","./react-18-DyM-o0Ps.js","./index-mQqIOHEI.js","./index-Q3IuyGkF.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js","./inheritsLoose-Y9jOMJLd.js","./index-LnHysSno.js","./index-BdDYuicC.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ assets/use_media_query-uEo48cPK.js
@@ -0,0 +1,20 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{M as r,C as s}from"./index-Q3IuyGkF.js";import{DemoExample as m}from"./use_media_query.stories-1hw5Yksf.js";import{useMDXComponents as i}from"./index-82O-B91n.js";import"./index-OjgoNOWw.js";import"./iframe-sYsqnhir.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js";import"./index-mQqIOHEI.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-LnHysSno.js";import"./index-BdDYuicC.js";import"./use_enhanced_effect-jDxBKzmp.js";function o(t){const n=Object.assign({h2:"h2",p:"p",pre:"pre",code:"code"},i(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Hooks/useMediaQuery"}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx(n.p,{children:"React hook that tracks state of a CSS media query."}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { useMediaQuery } from '@peculiar/react-components';
+
+const Demo = () => {
+  const isWide = useMediaQuery('(min-width: 480px)');
+
+  return (
+    <div>
+      Screen is wide:
+      {' '}
+      {isWide ? 'Yes' : 'No'}
+    </div>
+  );
+};
+`})}),`
+`,e.jsx(n.h2,{id:"demo",children:"Demo"}),`
+`,e.jsx(s,{of:m})]})}function C(t={}){const{wrapper:n}=Object.assign({},i(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{C as default};
