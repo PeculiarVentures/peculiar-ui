960ccd64f54e473954ae53874d4c5de552e3c626
donskov <donskov@users.noreply.github.com>
2024-02-27 13:25:27 +0000

    deploy: 48f0e5cdc2a32b3d07faf44e85bc7e9bb0669234

---
+++ assets/box.stories-2PIS3fGj.js
@@ -0,0 +1,44 @@
+import{j as r}from"./jsx-runtime-DtaoT6pD.js";import{B as o}from"./box-prmQx5g4.js";const W={title:"Components/Box",component:o,args:{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur",borderWidth:4,borderRadius:4,borderStyle:"solid",background:"primary-tint-3",borderColor:"secondary-tint-3"},argTypes:{children:{control:"text"},component:{control:!1}}},e={},d=()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{background:"attention",children:"Attention"}),r.jsx(o,{background:"gray-6",children:"Gray 6"})]}),t=()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{borderColor:"secondary",borderStyle:"solid",borderWidth:1,children:"Secondary"}),r.jsx(o,{borderColor:"wrong",borderStyle:"solid",borderWidth:4,children:"Wrong"})]}),i=()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,children:"Solid"}),r.jsx(o,{borderColor:"primary",borderStyle:"dashed",borderWidth:1,children:"Dashed"})]}),n=()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,children:"Default"}),r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,borderPosition:"horizontal",children:"Horizontal"}),r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,borderPosition:"vertical",children:"Vertical"}),r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,borderPosition:"top",children:"Top"}),r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,borderPosition:"bottom",children:"Bottom"}),r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,borderPosition:"left",children:"Left"}),r.jsx(o,{borderColor:"primary",borderStyle:"solid",borderWidth:1,borderPosition:"right",children:"Right"})]});var s,l,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(l=e.parameters)==null?void 0:l.docs)==null?void 0:a.source}}};var b,c,m;d.parameters={...d.parameters,docs:{...(b=d.parameters)==null?void 0:b.docs,source:{originalSource:`() => <>
+    <Box background="attention">
+      Attention
+    </Box>
+    <Box background="gray-6">
+      Gray 6
+    </Box>
+  </>`,...(m=(c=d.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,x,y;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`() => <>
+    <Box borderColor="secondary" borderStyle="solid" borderWidth={1}>
+      Secondary
+    </Box>
+    <Box borderColor="wrong" borderStyle="solid" borderWidth={4}>
+      Wrong
+    </Box>
+  </>`,...(y=(x=t.parameters)==null?void 0:x.docs)==null?void 0:y.source}}};var h,B,S;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`() => <>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1}>
+      Solid
+    </Box>
+    <Box borderColor="primary" borderStyle="dashed" borderWidth={1}>
+      Dashed
+    </Box>
+  </>`,...(S=(B=i.parameters)==null?void 0:B.docs)==null?void 0:S.source}}};var u,g,C;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`() => <>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1}>
+      Default
+    </Box>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1} borderPosition="horizontal">
+      Horizontal
+    </Box>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1} borderPosition="vertical">
+      Vertical
+    </Box>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1} borderPosition="top">
+      Top
+    </Box>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1} borderPosition="bottom">
+      Bottom
+    </Box>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1} borderPosition="left">
+      Left
+    </Box>
+    <Box borderColor="primary" borderStyle="solid" borderWidth={1} borderPosition="right">
+      Right
+    </Box>
+  </>`,...(C=(g=n.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};const j=["Playground","BackgroundColorExample","BorderColorExample","BorderStyleExample","BorderPositionExample"],E=Object.freeze(Object.defineProperty({__proto__:null,BackgroundColorExample:d,BorderColorExample:t,BorderPositionExample:n,BorderStyleExample:i,Playground:e,__namedExportsOrder:j,default:W},Symbol.toStringTag,{value:"Module"}));export{d as B,E as S,t as a,i as b,n as c};
---
+++ assets/text_field-XBO2Rhb9.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{M as i,C as t,A as d}from"./index-tfFjS02e.js";import{S as c,F as a,a as p,C as l,V as x}from"./text_field.stories-Mo9_bd5f.js";import{T as h}from"./text_field-wuxbIvIG.js";import{useMDXComponents as s}from"./index-82O-B91n.js";import"./index-OjgoNOWw.js";import"./iframe-2TuaWrW-.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js";import"./index-mQqIOHEI.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-LnHysSno.js";import"./index-BdDYuicC.js";import"./emotion-styled.browser.esm-jsqElTi7.js";import"./emotion-element-c39617d8.browser.esm-upVMHClS.js";import"./typography-be8ycqzM.js";function n(r){const o=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code"},s(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(o.h1,{id:"textfield",children:"TextField"}),`
+`,e.jsx(o.p,{children:"Text fields let users enter and edit text."}),`
+`,e.jsx(o.h2,{id:"form-props",children:"Form props"}),`
+`,e.jsx(t,{of:a}),`
+`,e.jsx(o.h2,{id:"size",children:"Size"}),`
+`,e.jsxs(o.p,{children:["Use the ",e.jsx(o.code,{children:"size"})," prop to change the size of the text field. You can set the value to ",e.jsx(o.code,{children:"small"}),", ",e.jsx(o.code,{children:"medium"})," (default) or ",e.jsx(o.code,{children:"large"}),"."]}),`
+`,e.jsx(t,{of:p}),`
+`,e.jsx(o.h2,{id:"color",children:"Color"}),`
+`,e.jsxs(o.p,{children:["Supports only ",e.jsx(o.code,{children:"secondary"})," color."]}),`
+`,e.jsx(t,{of:l}),`
+`,e.jsx(o.h2,{id:"validation",children:"Validation"}),`
+`,e.jsxs(o.p,{children:["The ",e.jsx(o.code,{children:"error"})," prop toggles the error state. The ",e.jsx(o.code,{children:"errorText"})," prop can then be used to provide feedback to the user about the error."]}),`
+`,e.jsx(t,{of:x}),`
+`,e.jsx(o.h2,{id:"component-api",children:"Component API"}),`
+`,e.jsx(d,{of:h})]})}function D(r={}){const{wrapper:o}=Object.assign({},s(),r.components);return o?e.jsx(o,Object.assign({},r,{children:e.jsx(n,r)})):n(r)}export{D as default};
