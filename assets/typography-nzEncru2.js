25b2ce78f35f2afd350753199217077389c048b8
donskov <donskov@users.noreply.github.com>
2024-02-27 10:51:24 +0000

    deploy: 8502d61a1cb9ea641ae96f7fb916c86db2193e69

---
+++ assets/typography-nzEncru2.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-DtaoT6pD.js";import{M as i,C as n,A as a}from"./index-6t12zupg.js";import{S as p,V as c,C as h,T as m}from"./typography.stories-KMKGgg5U.js";import{T as d}from"./typography-mH1J_rqf.js";import{useMDXComponents as s}from"./index-82O-B91n.js";import"./index-OjgoNOWw.js";import"./iframe-UxKXvYWL.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js";import"./index-mQqIOHEI.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-LnHysSno.js";import"./index-BdDYuicC.js";import"./emotion-styled.browser.esm-jsqElTi7.js";import"./emotion-element-c39617d8.browser.esm-upVMHClS.js";function r(t){const o=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",a:"a"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:p}),`
+`,e.jsx(o.h1,{id:"typography",children:"Typography"}),`
+`,e.jsx(o.p,{children:"Use typography to present your design and content as clearly and efficiently as possible."}),`
+`,e.jsx(o.h2,{id:"variant",children:"Variant"}),`
+`,e.jsx(n,{of:c}),`
+`,e.jsx(o.h2,{id:"color",children:"Color"}),`
+`,e.jsxs(o.p,{children:["Use the ",e.jsx(o.code,{children:"color"})," prop to change the color of the typography (see ",e.jsx(o.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,e.jsx(n,{of:h}),`
+`,e.jsx(o.h2,{id:"truncate-text",children:"Truncate text"}),`
+`,e.jsxs(o.p,{children:["Pass the ",e.jsx(o.code,{children:"noWrap"})," prop to render an ellipsis when the text exceeds the width of the viewport."]}),`
+`,e.jsx(n,{of:m}),`
+`,e.jsx(o.h2,{id:"component-api",children:"Component API"}),`
+`,e.jsx(a,{of:d})]})}function D(t={}){const{wrapper:o}=Object.assign({},s(),t.components);return o?e.jsx(o,Object.assign({},t,{children:e.jsx(r,t)})):r(t)}export{D as default};
