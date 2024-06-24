64a776a4650073c4870f55c7ce4e5d6e1dd3f06d
donskov <donskov@users.noreply.github.com>
2024-06-24 16:13:34 +0000

    deploy: 8cf11adce312851c6774914466078d3930aaf7cb

---
+++ assets/preview-zf-ZiJc9.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-OHVPl2j9.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-iG1MzMfW.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-iG1MzMfW.js","./chunk-HLWAVYOI-GUIc3XWW.js","./iframe-OHVPl2j9.js","./index-OjgoNOWw.js","./react-18-DyM-o0Ps.js","./index-mQqIOHEI.js","./index-y0by1Lln.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js","./inheritsLoose-Y9jOMJLd.js","./index-LnHysSno.js","./index-BdDYuicC.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
