2f6ff4dfc9a94c80e826f1155cacf842041dfa3a
donskov <donskov@users.noreply.github.com>
2024-08-29 15:51:53 +0000

    deploy: d4ce960d4a43a904c2004e5222a21bf30010ae20

---
+++ assets/box-WUxatKk6.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-DtaoT6pD.js";import{M as i,C as n,A as c}from"./index-UF7dRslt.js";import{S as d,B as h,a as p,b as a,c as x,d as l}from"./box.stories-tWlYbFhl.js";import{useMDXComponents as t}from"./index-82O-B91n.js";import{B as m}from"./box-bq1vF_Hs.js";import"./index-OjgoNOWw.js";import"./iframe-7mXulWtI.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js";import"./index-mQqIOHEI.js";import"./inheritsLoose-Y9jOMJLd.js";import"./index-LnHysSno.js";import"./index-BdDYuicC.js";import"./emotion-styled.browser.esm-jsqElTi7.js";import"./emotion-element-c39617d8.browser.esm-upVMHClS.js";function s(r){const e=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",a:"a",h4:"h4"},t(),r.components);return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:d}),`
+`,o.jsx(e.h1,{id:"box",children:"Box"}),`
+`,o.jsxs(e.p,{children:["The Box component serves as a wrapper component for quickly change ",o.jsx(e.code,{children:"background"})," and ",o.jsx(e.code,{children:"border"})," properties."]}),`
+`,o.jsx(e.h2,{id:"background",children:"Background"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"background"})," prop to change the color of the Box background color (see ",o.jsx(e.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,o.jsx(n,{of:h}),`
+`,o.jsx(e.h2,{id:"border",children:"Border"}),`
+`,o.jsx(e.h4,{id:"border-color",children:"Border color"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"borderColor"})," prop to change the color of the Box border (see ",o.jsx(e.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(e.h4,{id:"border-style",children:"Border style"}),`
+`,o.jsx(n,{of:a}),`
+`,o.jsx(e.h4,{id:"border-position",children:"Border position"}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(e.h2,{id:"shadow",children:"Shadow"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"boxShadow"})," prop to change the shadow of the Box (see ",o.jsx(e.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(e.h2,{id:"component-api",children:"Component API"}),`
+`,o.jsx(c,{of:m})]})}function O(r={}){const{wrapper:e}=Object.assign({},t(),r.components);return e?o.jsx(e,Object.assign({},r,{children:o.jsx(s,r)})):s(r)}export{O as default};
---
+++ assets/preview-HVKfN0WO.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-7mXulWtI.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-YG92II2S.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-YG92II2S.js","./chunk-HLWAVYOI-5bJmx55w.js","./iframe-7mXulWtI.js","./index-OjgoNOWw.js","./react-18-DyM-o0Ps.js","./index-mQqIOHEI.js","./index-UF7dRslt.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js","./inheritsLoose-Y9jOMJLd.js","./index-LnHysSno.js","./index-BdDYuicC.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
