69d16ba152929b33d9ad91bac802c932d38c4b5c
donskov <donskov@users.noreply.github.com>
2024-09-10 10:09:48 +0000

    deploy: 3b5136cc8aec8d4a2bcb9b2a5928ffef5f5f4843

--- assets/preview-xi9rapnE.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-Di3G6qoa.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-cP9UsLX_.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-cP9UsLX_.js","./chunk-HLWAVYOI-Ycm5PLJ1.js","./iframe-Di3G6qoa.js","./index-OjgoNOWw.js","./react-18-DyM-o0Ps.js","./index-mQqIOHEI.js","./index-FEzJ-IFI.js","./emotion-use-insertion-effect-with-fallbacks.browser.esm-xK8HOd_o.js","./inheritsLoose-Y9jOMJLd.js","./index-LnHysSno.js","./index-BdDYuicC.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
