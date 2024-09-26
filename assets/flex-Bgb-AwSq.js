a1a8aa6d12de021ff441a3332df380798d9b1f23
donskov <donskov@users.noreply.github.com>
2024-09-26 11:51:55 +0000

    deploy: cb33874dc0404e0d3fb8d18eddcc13395774641c

---
+++ assets/flex-Bgb-AwSq.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-Du8NFWEI.js";import{M as r,C as n,A as i}from"./index-CqtPok8e.js";import{S as l,A as h,V as m,C as c}from"./flex.stories-EXq9nYOK.js";import{F as x,a as p}from"./flex_container-D1TdyU-p.js";import{useMDXComponents as a}from"./index-CL8UvjIf.js";import"./index-Dl6G-zuu.js";import"./iframe-DQIaFMqr.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";import"./box-fmBxd1a2.js";import"./emotion-styled.browser.esm-Cx145QRj.js";import"./emotion-element-c39617d8.browser.esm-DM69r4Qw.js";function s(o){const e=Object.assign({h1:"h1",h2:"h2",p:"p",code:"code",h3:"h3"},a(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:l}),`
+`,t.jsx(e.h1,{id:"flex",children:"Flex"}),`
+`,t.jsx(e.h2,{id:"auto-layout",children:"Auto-layout"}),`
+`,t.jsx(e.p,{children:"The auto-layout feature gives equal space to all items present. When you set the width of one item, the others will automatically resize to match it."}),`
+`,t.jsx(n,{of:h}),`
+`,t.jsx(e.h2,{id:"variable-width-content",children:"Variable width content"}),`
+`,t.jsxs(e.p,{children:["When a breakpoint's value is given as ",t.jsx(e.code,{children:"auto"}),", then a column's size will automatically adjust to match the width of its content. The demo below shows how this works:"]}),`
+`,t.jsx(n,{of:m}),`
+`,t.jsx(e.h2,{id:"column",children:"Column"}),`
+`,t.jsx(n,{of:c}),`
+`,t.jsx(e.h2,{id:"component-api",children:"Component API"}),`
+`,t.jsx(e.h3,{id:"flexcontainer",children:"FlexContainer"}),`
+`,t.jsx(i,{of:x}),`
+`,t.jsx(e.h3,{id:"flex-1",children:"Flex"}),`
+`,t.jsx(i,{of:p})]})}function T(o={}){const{wrapper:e}=Object.assign({},a(),o.components);return e?t.jsx(e,Object.assign({},o,{children:t.jsx(s,o)})):s(o)}export{T as default};
---
+++ assets/use_clipboard-BnG-tYDn.js
@@ -0,0 +1,21 @@
+import{j as o}from"./jsx-runtime-Du8NFWEI.js";import{M as r,C as s}from"./index-CqtPok8e.js";import{DemoExample as p}from"./use_clipboard.stories-B064ckxh.js";import{useMDXComponents as i}from"./index-CL8UvjIf.js";import"./index-Dl6G-zuu.js";import"./iframe-DQIaFMqr.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";function t(e){const n=Object.assign({h2:"h2",p:"p",pre:"pre",code:"code"},i(),e.components);return o.jsxs(o.Fragment,{children:[o.jsx(r,{title:"Hooks/useClipboard"}),`
+`,o.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,o.jsx(n.p,{children:"React hook that provides copy to clipboard functionality."}),`
+`,o.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,o.jsx(n.pre,{children:o.jsx(n.code,{className:"language-jsx",children:`import { useClipboard } from '@peculiar/react-components';
+
+export const Demo = () => {
+  const { copy, isCopied } = useClipboard();
+
+  return (
+    <button
+      type="button"
+      onClick={() => copy('Text to copy')}
+    >
+      {isCopied ? 'Copied' : 'Click to copy'}
+    </button>
+  );
+};
+`})}),`
+`,o.jsx(n.h2,{id:"demo",children:"Demo"}),`
+`,o.jsx(s,{of:p})]})}function g(e={}){const{wrapper:n}=Object.assign({},i(),e.components);return n?o.jsx(n,Object.assign({},e,{children:o.jsx(t,e)})):t(e)}export{g as default};
