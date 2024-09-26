5523ae7e93feaab7473113614677febc7f29f30d
donskov <donskov@users.noreply.github.com>
2024-09-26 16:38:35 +0000

    deploy: 60f39d2dadbd761f2f012b86eff07ab0836c41c5

---
+++ assets/palette-Cb7Nm-2V.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Du8NFWEI.js";import{M as i}from"./index-DQK4iNk9.js";import{ColorItemExample as o}from"./palette.stories-B2l80gDb.js";import{useMDXComponents as r}from"./index-CL8UvjIf.js";import{d as a,i as l,j as m,k as c,l as p,m as j,n as x}from"./utils-DDmc6yYE.js";import"./index-Dl6G-zuu.js";import"./iframe-Bgj4Woy0.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";import"./typography-6yWOy3Td.js";import"./emotion-styled.browser.esm-Cx145QRj.js";import"./emotion-element-c39617d8.browser.esm-DM69r4Qw.js";function n(s){const e=Object.assign({h1:"h1",p:"p",h2:"h2"},r(),s.components);return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Customization/Palette"}),`
+`,t.jsx(e.h1,{id:"palette",children:"Palette"}),`
+`,t.jsx(e.p,{children:"The palette enables you to modify the color of the components to suit your brand."}),`
+`,t.jsx(e.h2,{id:"colors",children:"Colors"}),`
+`,t.jsx(o,{title:"Primary",colors:a}),`
+`,t.jsx(o,{title:"Secondary",colors:l}),`
+`,t.jsx(o,{title:"Wrong",colors:m}),`
+`,t.jsx(o,{title:"Attention",colors:c}),`
+`,t.jsx(o,{title:"Success",colors:p}),`
+`,t.jsx(o,{title:"Grayscale",colors:j}),`
+`,t.jsx(o,{title:"Additional",colors:x})]})}function S(s={}){const{wrapper:e}=Object.assign({},r(),s.components);return e?t.jsx(e,Object.assign({},s,{children:t.jsx(n,s)})):n(s)}export{S as default};
