265b4b7db79d3fc2f0700cdce4549bea9f20a826
donskov <donskov@users.noreply.github.com>
2024-10-24 10:20:34 +0000

    deploy: 9b7d39e72bd245cbf5000dd544dc9a19c5a2a3ae

---
+++ assets/circular_progress-BhiYwC6M.js
@@ -0,0 +1,23 @@
+import{j as o}from"./jsx-runtime-Du8NFWEI.js";import{R as f}from"./index-Dl6G-zuu.js";import{k as c}from"./emotion-react.browser.esm-0eqfKSP6.js";import{n,i as v}from"./emotion-styled.browser.esm-Cx145QRj.js";const h=c`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`,y=c`
+  0% {
+    stroke-dasharray: 1px, 200px;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 100px, 200px;
+    stroke-dashoffset: -15px;
+  }
+  100% {
+    stroke-dasharray: 100px, 200px;
+    stroke-dashoffset: -125px;
+  }
+`,g=n("div",{shouldForwardProp:e=>v(e)&&e!=="color"})(e=>({label:"CircularProgress",overflow:"hidden",position:"relative",display:"inline-block",color:`var(--pv-color-${e.color})`,...e.variant==="indeterminate"?{animation:`${h} 1.4s linear infinite`}:{transform:"rotate(-90deg)"},...e.size==="small"&&{height:"var(--pv-size-base-3)",width:"var(--pv-size-base-3)"},...e.size==="large"&&{height:"var(--pv-size-base-6)",width:"var(--pv-size-base-6)"}})),x=n("svg")({display:"block"}),k=n("circle")(e=>({stroke:"currentcolor",strokeDasharray:"80px, 200px",strokeDashoffset:0,...e.variant==="indeterminate"&&{animation:`${y} 1.4s ease-in-out infinite`}})),r=44,i=4,a=f.forwardRef((e,d)=>{const{size:u,variant:t="indeterminate",value:m=0,...p}=e,s={};if(t==="determinate"){const l=2*Math.PI*((r-i)/2);s.strokeDasharray=l.toFixed(3),s.strokeDashoffset=`${((100-m)/100*l).toFixed(3)}px`}return o.jsx(g,{ref:d,role:"progressbar",variant:t,size:u,...p,children:o.jsx(x,{viewBox:`${r/2} ${r/2} ${r} ${r}`,children:o.jsx(k,{cx:r,cy:r,r:(r-i)/2,fill:"none",strokeWidth:i,style:s,variant:t})})})});a.displayName="CircularProgress";a.defaultProps={color:"primary",size:"large",variant:"indeterminate",value:0};try{a.displayName="CircularProgress",a.__docgenInfo={description:"",displayName:"CircularProgress",props:{color:{defaultValue:{value:"primary"},description:"The color of the component. It supports those theme colors that make sense for this component.",name:"color",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"secondary"'},{value:'"white"'}]}},className:{defaultValue:null,description:"The className of the component.",name:"className",required:!1,type:{name:"string"}},size:{defaultValue:{value:"large"},description:"The size of the progress.",name:"size",required:!1,type:{name:"enum",value:[{value:'"small"'},{value:'"large"'}]}},variant:{defaultValue:{value:"indeterminate"},description:`The variant to use.
+Use indeterminate when there is no progress value.`,name:"variant",required:!1,type:{name:"enum",value:[{value:'"indeterminate"'},{value:'"determinate"'}]}},value:{defaultValue:{value:"0"},description:`The value of the progress indicator for the determinate variant.
+Value between 0 and 100.`,name:"value",required:!1,type:{name:"number"}}}}}catch{}export{a as C};
---
+++ assets/installation-Dit_owvC.js
@@ -0,0 +1,22 @@
+import{j as e}from"./jsx-runtime-Du8NFWEI.js";import{M as i}from"./index-DxLa_1_D.js";import{useMDXComponents as t}from"./index-icA9A9Lr.js";import"./index-Dl6G-zuu.js";import"./iframe-C7L8w2jw.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";function r(o){const n=Object.assign({h1:"h1",p:"p",code:"code",a:"a",pre:"pre"},t(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Installation"}),`
+`,e.jsx(n.h1,{id:"installation",children:"Installation"}),`
+`,e.jsxs(n.p,{children:["To install ",e.jsx(n.code,{children:"@peculiar/react-components"}),` in your project, you will need to run the
+following command using `,e.jsx(n.a,{href:"https://www.npmjs.com/",target:"_blank",rel:"nofollow noopener noreferrer",children:"npm"}),":"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`npm i @peculiar/react-components
+`})}),`
+`,e.jsxs(n.p,{children:["If you prefer ",e.jsx(n.a,{href:"https://yarnpkg.com/en/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Yarn"}),", use the following command instead:"]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`yarn add @peculiar/react-components
+`})}),`
+`,e.jsxs(n.p,{children:["After installing, you need to set up the ThemeProvider at the root of your application. This can be either in your ",e.jsx(n.code,{children:"index.jsx"}),", ",e.jsx(n.code,{children:"index.tsx"})," or ",e.jsx(n.code,{children:"App.jsx"})," depending on the framework you use."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`import { Button, ThemeProvider } from '@peculiar/react-components';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Button>
+        Action
+      </Button>
+    </ThemeProvider>
+  );
+}
+`})})]})}function f(o={}){const{wrapper:n}=Object.assign({},t(),o.components);return n?e.jsx(n,Object.assign({},o,{children:e.jsx(r,o)})):r(o)}export{f as default};
