464fd5dc79abe1007d6e76dcd2819efef36807ae
donskov <donskov@users.noreply.github.com>
2024-10-25 08:03:23 +0000

    deploy: 3c1e8fc860c10594f784484d1b48994b2708a63c

---
+++ assets/use_controllable-mHxY4HDh.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-Du8NFWEI.js";import{M as a,C as s}from"./index-CahDekZF.js";import{DemoExample as l}from"./use_controllable.stories-CzBZw3eQ.js";import{useMDXComponents as r}from"./index-icA9A9Lr.js";import"./index-Dl6G-zuu.js";import"./iframe-D9NETBGr.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";import"./use_controllable-DUuBybTH.js";function o(t){const e=Object.assign({h2:"h2",p:"p",pre:"pre",code:"code"},r(),t.components);return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Hooks/useControllableState"}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx(e.p,{children:"React hook that allows any component handle controlled and uncontrolled modes, and provide control over its internal state."}),`
+`,n.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-jsx",children:`import { useControllableState } from '@peculiar/react-components';
+
+export const Demo = () => {
+  const [value, setValue] = useControllableState({ defaultValue: 40 });
+
+  return (
+    <div>
+      <button
+        onClick={() => setValue(value + 1)}
+        type="button"
+      >
+        +
+      </button>
+      <span>
+        {value}
+      </span>
+      <button
+        onClick={() => setValue(value - 1)}
+        type="button"
+      >
+        -
+      </button>
+    </div>
+  );
+};
+`})}),`
+`,n.jsx(e.h2,{id:"demo",children:"Demo"}),`
+`,n.jsx(s,{of:l})]})}function g(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n.jsx(e,Object.assign({},t,{children:n.jsx(o,t)})):o(t)}export{g as default};
---
+++ assets/use_intersection_observer-mHKDe6kN.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-Du8NFWEI.js";import{M as i,C as s}from"./index-CahDekZF.js";import{DemoExample as c}from"./use_intersection_observer.stories-pZYYa9r2.js";import{useMDXComponents as r}from"./index-icA9A9Lr.js";import"./index-Dl6G-zuu.js";import"./iframe-D9NETBGr.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";function o(t){const n=Object.assign({h2:"h2",p:"p",pre:"pre",code:"code"},r(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Hooks/useIntersectionObserver"}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx(n.p,{children:"React hook detects visibility of a component on the viewport using the natively present in the browser."}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { useIntersectionObserver } from '@peculiar/react-components';
+
+const Section = (props: { title: string }) => {
+  const { title } = props;
+  const [refIntersecting, { isIntersecting }] = useIntersectionObserver();
+
+  console.log(\`Render Section \${title}\`, { isIntersecting });
+
+  return (
+    <div
+      ref={refIntersecting}
+      style={{
+        height: '20vh',
+        display: 'flex',
+        border: '1px dashed #000',
+        fontSize: '2rem',
+      }}
+    >
+      <div style={{ margin: 'auto' }}>
+        {title}
+      </div>
+    </div>
+  );
+};
+
+export const Demo = () => (
+  Array.from({ length: 5 }).map((_, index) => (
+    <Section
+      key={index}
+      title={\`\${index + 1}\`}
+    />
+  ))
+);
+`})}),`
+`,e.jsx(n.h2,{id:"demo",children:"Demo"}),`
+`,e.jsx(s,{of:c,sourceState:"none"})]})}function b(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e.jsx(n,Object.assign({},t,{children:e.jsx(o,t)})):o(t)}export{b as default};
