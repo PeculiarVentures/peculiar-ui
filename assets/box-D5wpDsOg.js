fe488d10b7adc9adefafc1a70e04667bd9838940
donskov <donskov@users.noreply.github.com>
2024-11-19 15:23:30 +0000

    deploy: 3f2d7824a436c361bd36e88641921a2cd2f6c4b6

---
+++ assets/box-D5wpDsOg.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-Du8NFWEI.js";import{M as i,C as n,A as c}from"./index-B63_OtA-.js";import{S as d,B as h,a as p,b as a,c as x,d as l}from"./box.stories-w1PdxPoi.js";import{useMDXComponents as t}from"./index-icA9A9Lr.js";import{B as m}from"./box-fmBxd1a2.js";import"./index-Dl6G-zuu.js";import"./iframe-Citu-EE5.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";import"./emotion-styled.browser.esm-Cx145QRj.js";import"./emotion-element-c39617d8.browser.esm-DM69r4Qw.js";function s(r){const e=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",a:"a",h4:"h4"},t(),r.components);return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:d}),`
+`,o.jsx(e.h1,{id:"box",children:"Box"}),`
+`,o.jsxs(e.p,{children:["The Box component serves as a wrapper component for quickly change ",o.jsx(e.code,{children:"background"})," and ",o.jsx(e.code,{children:"border"})," properties."]}),`
+`,o.jsx(e.h2,{id:"background",children:"Background"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"background"})," prop to change the color of the Box background color (see ",o.jsx(e.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,o.jsx(n,{of:h}),`
+`,o.jsx(e.h2,{id:"border",children:"Border"}),`
+`,o.jsx(e.h4,{id:"border-color",children:"Border color"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"borderColor"})," prop to change the color of the Box border (see ",o.jsx(e.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(e.h4,{id:"border-style",children:"Border style"}),`
+`,o.jsx(n,{of:a}),`
+`,o.jsx(e.h4,{id:"border-position",children:"Border position"}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(e.h2,{id:"shadow",children:"Shadow"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"boxShadow"})," prop to change the shadow of the Box (see ",o.jsx(e.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(e.h2,{id:"component-api",children:"Component API"}),`
+`,o.jsx(c,{of:m})]})}function O(r={}){const{wrapper:e}=Object.assign({},t(),r.components);return e?o.jsx(e,Object.assign({},r,{children:o.jsx(s,r)})):s(r)}export{O as default};
---
+++ assets/button-DnwC_m8u.js
@@ -0,0 +1,31 @@
+import{j as o}from"./jsx-runtime-Du8NFWEI.js";import{M as s,C as n,A as c}from"./index-B63_OtA-.js";import{S as a,T as l,C as d,O as h,a as x,D as m,P as j,b as p,W as u,c as f,I as b}from"./button.stories-DC5vtgsU.js";import{B as g}from"./button-B0Y9Yi1I.js";import{useMDXComponents as i}from"./index-icA9A9Lr.js";import"./index-Dl6G-zuu.js";import"./iframe-Citu-EE5.js";import"../sb-preview/runtime.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-L5l4RKBO.js";import"./index-D1_ZHIBm.js";import"./inheritsLoose-B7h9gheN.js";import"./index-B5O4V4BP.js";import"./index-B9pQ14RZ.js";import"./plus_icon-BJbj7At9.js";import"./close_icon-pwUgUdvH.js";import"./emotion-styled.browser.esm-Cx145QRj.js";import"./emotion-element-c39617d8.browser.esm-DM69r4Qw.js";import"./button_base-7MZVKYUz.js";import"./typography-6yWOy3Td.js";function r(t){const e=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",h4:"h4"},i(),t.components);return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:a}),`
+`,o.jsx(e.h1,{id:"button",children:"Button"}),`
+`,o.jsx(e.p,{children:"Buttons allow users to take actions, and make choices, with a single tap."}),`
+`,o.jsx(e.h2,{id:"variant",children:"Variant"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"Button"})," comes with three variants: ",o.jsx(e.code,{children:"text"})," (default), ",o.jsx(e.code,{children:"contained"}),", and ",o.jsx(e.code,{children:"outlined"}),"."]}),`
+`,o.jsx(e.h4,{id:"text-button",children:"Text button"}),`
+`,o.jsx(n,{of:l}),`
+`,o.jsx(e.h4,{id:"contained-button",children:"Contained button"}),`
+`,o.jsx(n,{of:d}),`
+`,o.jsx(e.h4,{id:"outlined-button",children:"Outlined button"}),`
+`,o.jsx(n,{of:h}),`
+`,o.jsx(e.h2,{id:"size",children:"Size"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"size"})," prop to change the size of the button. You can set the value to ",o.jsx(e.code,{children:"small"}),", ",o.jsx(e.code,{children:"medium"})," (default) or ",o.jsx(e.code,{children:"large"}),"."]}),`
+`,o.jsx(n,{of:x}),`
+`,o.jsx(e.h2,{id:"color",children:"Color"}),`
+`,o.jsxs(e.p,{children:["Use the ",o.jsx(e.code,{children:"color"})," prop to change the color of the button. You can set the value to ",o.jsx(e.code,{children:"default"})," (default), ",o.jsx(e.code,{children:"primary"}),", ",o.jsx(e.code,{children:"secondary"}),", ",o.jsx(e.code,{children:"wrong"})," or ",o.jsx(e.code,{children:"white"}),"."]}),`
+`,o.jsx(e.h4,{id:"default-color",children:"Default color"}),`
+`,o.jsx(n,{of:m}),`
+`,o.jsx(e.h4,{id:"primary-color",children:"Primary color"}),`
+`,o.jsx(n,{of:j}),`
+`,o.jsx(e.h4,{id:"secondary-color",children:"Secondary color"}),`
+`,o.jsx(n,{of:p}),`
+`,o.jsx(e.h4,{id:"wrong-color",children:"Wrong color"}),`
+`,o.jsx(n,{of:u}),`
+`,o.jsx(e.h4,{id:"white-color",children:"White color"}),`
+`,o.jsx(n,{of:f,className:"bg-dark"}),`
+`,o.jsx(e.h2,{id:"icon-button",children:"Icon button"}),`
+`,o.jsx(e.p,{children:"Sometimes you might want to have icons for certain buttons to enhance the UX of the application as we recognize logos more easily than plain text."}),`
+`,o.jsx(n,{of:b}),`
+`,o.jsx(e.h2,{id:"component-api",children:"Component API"}),`
+`,o.jsx(c,{of:g})]})}function Y(t={}){const{wrapper:e}=Object.assign({},i(),t.components);return e?o.jsx(e,Object.assign({},t,{children:o.jsx(r,t)})):r(t)}export{Y as default};
