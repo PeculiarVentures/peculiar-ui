22b2d05942620e723d87b981c32bb1b4cb6e2e3e
donskov <donskov@users.noreply.github.com>
2024-12-16 08:32:16 +0000

    deploy: 3f802848fef5a1744d237b737a6d4b0284a94a77

--- assets/DocsRenderer-CFRXHY34-C9UwWYzz.js
+++
@@ -1,1 +0,0 @@
-import{D as f,d as D}from"./chunk-NUUEMKO5-B3I_shGN.js";import"./iframe-DQmigF-8.js";import"../sb-preview/runtime.js";import"./index-Wp2u197Z.js";import"./index-OEU5mBnJ.js";import"./jsx-runtime-CQsLhzk5.js";import"./index-DA8gG4lw.js";import"./index-yiGAEVS0.js";import"./index-Dy31kHqt.js";import"./react-18-Bstx5tUr.js";export{f as DocsRenderer,D as defaultComponents};
---
+++ assets/use_intersection_observer-zzheKQvj.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as r}from"./index-C2WH5l5l.js";import{ae as i,af as s}from"./index-Cdy_k4wO.js";import{DemoExample as c}from"./use_intersection_observer.stories-JeRiX9AL.js";import"./index-Wp2u197Z.js";import"./iframe-IXF6vH_u.js";import"../sb-preview/runtime.js";import"./index-DA8gG4lw.js";import"./index-yiGAEVS0.js";import"./index-Dy31kHqt.js";function o(t){const n={code:"code",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Hooks/useIntersectionObserver"}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsx(n.p,{children:"React hook detects visibility of a component on the viewport using the natively present in the browser."}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { useIntersectionObserver } from '@peculiar/react-components';
+
+const Section = (props: { title: string }) => {
+  const { title } = props;
+  const [refIntersecting, { isIntersecting }] = useIntersectionObserver();
+
+  console.log(\`Render Section \${title}\`, { isIntersecting });
+
+  return (
+    <div
+      ref={refIntersecting}
+      style={{
+        height: '20vh',
+        display: 'flex',
+        border: '1px dashed #000',
+        fontSize: '2rem',
+      }}
+    >
+      <div style={{ margin: 'auto' }}>
+        {title}
+      </div>
+    </div>
+  );
+};
+
+export const Demo = () => (
+  Array.from({ length: 5 }).map((_, index) => (
+    <Section
+      key={index}
+      title={\`\${index + 1}\`}
+    />
+  ))
+);
+`})}),`
+`,e.jsx(n.h2,{id:"demo",children:"Demo"}),`
+`,e.jsx(s,{of:c,sourceState:"none"})]})}function g(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{g as default};
