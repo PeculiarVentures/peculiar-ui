e1c3cf66886368606aca668b59a3ca10269e73d8
donskov <donskov@users.noreply.github.com>
2025-01-03 10:48:02 +0000

    deploy: 7f7c6598abd2a7b60f07cf2ff9f0d67f9c6a5154

---
+++ assets/use_controllable-5IVSUcOZ.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-CQsLhzk5.js";import{useMDXComponents as r}from"./index-C2WH5l5l.js";import{ae as a,af as s}from"./index-BUlNSo9C.js";import{DemoExample as l}from"./use_controllable.stories-DQUTFiII.js";import"./index-Wp2u197Z.js";import"./iframe-B1OxMBR5.js";import"../sb-preview/runtime.js";import"./index-DA8gG4lw.js";import"./index-yiGAEVS0.js";import"./index-Dy31kHqt.js";import"./use_controllable-DRJrQW8S.js";function o(t){const e={code:"code",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Hooks/useControllableState"}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx(e.p,{children:"React hook that allows any component handle controlled and uncontrolled modes, and provide control over its internal state."}),`
+`,n.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-jsx",children:`import { useControllableState } from '@peculiar/react-components';
+
+export const Demo = () => {
+  const [value, setValue] = useControllableState({ defaultValue: 40 });
+
+  return (
+    <div>
+      <button
+        onClick={() => setValue(value + 1)}
+        type="button"
+      >
+        +
+      </button>
+      <span>
+        {value}
+      </span>
+      <button
+        onClick={() => setValue(value - 1)}
+        type="button"
+      >
+        -
+      </button>
+    </div>
+  );
+};
+`})}),`
+`,n.jsx(e.h2,{id:"demo",children:"Demo"}),`
+`,n.jsx(s,{of:l})]})}function b(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{b as default};
