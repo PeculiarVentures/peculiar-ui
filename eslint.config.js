879ad12cd53df62432dd8ebc123fb26b368c5720
Mykhailo Kostov <mykhailo.kostov@peculiarventures.com>
2025-01-09 18:08:30 +0200

    chore: Update eslint from v8.46.0 to v9.17.0

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+export default tseslint.config([
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  jsxA11y.flatConfigs.recommended,
+  reactPlugin.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      ...reactHooksPlugin.configs.recommended.rules,
+    },
+  },
+]);
