919b67661765c885dc44a2fd32d7d0e557718470
Mykhailo Kostov <mykhailo.kostov@peculiarventures.com>
2025-01-15 13:04:03 +0200

    chore(eslint.config.mjs): Install @peculiar-config-react/base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,14 @@
-import eslint from '@eslint/js';
 // eslint-disable-next-line import/no-unresolved
 import tseslint from 'typescript-eslint';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import stylistic from '@stylistic/eslint-plugin';
-import importPlugin from 'eslint-plugin-import';
+import baseConfig from '@peculiar/eslint-config-base';
+import reactConfig from '@peculiar/eslint-config-react';
 
 export default tseslint.config([
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  jsxA11y.flatConfigs.recommended,
-  reactPlugin.configs.flat.recommended,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
-  stylistic.configs.customize({
-    semi: true,
-    indent: 2,
-    quotes: 'single',
-    jsx: true,
-    arrowParens: true,
-    braceStyle: '1tbs',
-  }),
+  ...baseConfig,
+  ...reactConfig,
   {
-    plugins: {
-      'react-hooks': reactHooksPlugin,
-    },
     rules: {
-      ...reactHooksPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
   {
@@ -36,87 +16,5 @@ export default tseslint.config([
       '**/dist/*',
       '**/public/*',
     ],
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@stylistic/padding-line-between-statements': [
-        'error',
-        {
-          blankLine: 'always',
-          prev: '*',
-          next: [
-            'return',
-            'throw',
-            'break',
-            'continue',
-            'export',
-          ],
-        },
-        {
-          blankLine: 'always',
-          prev: [
-            'block-like',
-            'for',
-            'switch',
-            'try',
-            'while',
-            'case',
-            'default',
-            'const',
-            'let',
-            'var',
-            'import',
-          ],
-          next: '*',
-        },
-        {
-          blankLine: 'any',
-          prev: ['const', 'let', 'var'],
-          next: ['const', 'let', 'var'],
-        },
-        {
-          blankLine: 'never',
-          prev: 'import',
-          next: 'import',
-        },
-        {
-          blankLine: 'any',
-          prev: 'export',
-          next: 'export',
-        },
-      ],
-      '@stylistic/jsx-max-props-per-line': [
-        'error',
-        {
-          maximum: 2,
-        },
-      ],
-      '@stylistic/object-curly-newline': [
-        'error',
-        {
-          ObjectExpression: {
-            multiline: true,
-            minProperties: 1,
-          },
-          ObjectPattern: {
-            multiline: true,
-            minProperties: 1,
-          },
-          ImportDeclaration: {
-            multiline: true,
-            minProperties: 3,
-          },
-          ExportDeclaration: {
-            multiline: true,
-            minProperties: 3,
-          },
-        },
-      ],
-      '@stylistic/jsx-function-call-newline': [
-        'error',
-        'always',
-      ],
-    },
   },
 ]);
