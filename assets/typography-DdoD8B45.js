3057cd99a5310550c821e45e09d4de76c95d6e8c
donskov <donskov@users.noreply.github.com>
2025-10-15 09:29:15 +0000

    deploy: 4cf59b9c2c37af1fb75ad6d5c53283688fb1f875

---
+++ assets/typography-DdoD8B45.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-xF634gn_.js";import{useMDXComponents as s}from"./index-o2KxC7bF.js";import{M as p,C as t,A as a}from"./index-DyAqIPDL.js";import{S as i,V as c,C as h,T as d}from"./typography.stories-BuCYMmxm.js";import{T as m}from"./typography-CX2mak6k.js";import"./index-C-7etoUd.js";import"./iframe-BYHPBPv0.js";import"./index-dzNl0v_V.js";import"./index-DWe4bgYs.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./emotion-styled.browser.esm-D5cirWhA.js";import"./emotion-element-43c6fea0.browser.esm-BY9Huquz.js";function r(n){const o={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(p,{of:i}),`
+`,e.jsx(o.h1,{id:"typography",children:"Typography"}),`
+`,e.jsx(o.p,{children:"Use typography to present your design and content as clearly and efficiently as possible."}),`
+`,e.jsx(o.h2,{id:"variant",children:"Variant"}),`
+`,e.jsx(t,{of:c}),`
+`,e.jsx(o.h2,{id:"color",children:"Color"}),`
+`,e.jsxs(o.p,{children:["Use the ",e.jsx(o.code,{children:"color"})," prop to change the color of the typography (see ",e.jsx(o.a,{href:"#component-api",children:"props"})," section)."]}),`
+`,e.jsx(t,{of:h}),`
+`,e.jsx(o.h2,{id:"truncate-text",children:"Truncate text"}),`
+`,e.jsxs(o.p,{children:["Pass the ",e.jsx(o.code,{children:"noWrap"})," prop to render an ellipsis when the text exceeds the width of the viewport."]}),`
+`,e.jsx(t,{of:d}),`
+`,e.jsx(o.h2,{id:"component-api",children:"Component API"}),`
+`,e.jsx(a,{of:m})]})}function A(n={}){const{wrapper:o}={...s(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(r,{...n})}):r(n)}export{A as default};
