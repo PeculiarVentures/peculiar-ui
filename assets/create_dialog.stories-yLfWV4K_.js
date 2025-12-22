715f840a15fec3c721123db37debe85c1903c829
donskov <donskov@users.noreply.github.com>
2025-12-22 09:26:59 +0000

    deploy: 775cb5dfdc10454b264c83dad060029c7ede56eb

---
+++ assets/create_dialog.stories-yLfWV4K_.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-xF634gn_.js";import{e as t}from"./index-C-7etoUd.js";import{B as m}from"./button-NTqqnq1M.js";import{D as f,a as C,b as D,c as x}from"./dialog_actions-CP6xJABN.js";import"./emotion-styled.browser.esm-CnX2NPaZ.js";import"./emotion-element-f0de968e.browser.esm-DLlpVLeB.js";import"./button_base-CeL69T3V.js";import"./typography-D-u68OCN.js";import"./fade-DHl67KFq.js";import"./use_merged_ref-ClS6-mGQ.js";import"./Transition-RLbX08wD.js";import"./index-dzNl0v_V.js";import"./index-DWe4bgYs.js";import"./circular_progress-BWunMd4w.js";import"./emotion-react.browser.esm-vU_FEsrj.js";import"./modal-hlOGmFQp.js";import"./portal-B1al-bvw.js";import"./use_enhanced_effect-B1RabiBj.js";import"./box-9womTain.js";import"./collapse-Dy8w92x5.js";import"./alert-hxycwxGh.js";import"./icon_button-B-d6mlLU.js";import"./tooltip-CPISR9CN.js";import"./use_controllable-DXs9Mygf.js";import"./popper-BrnnUaNn.js";import"./usePopper-CdO4NS3w.js";import"./close_icon-DLJ9YGil.js";import"./flex_container-CrE31eS_.js";import"./close_circle_icon-Nt97DkBN.js";import"./warning_icon-D6xCuvw-.js";function p(n){return function(){const[i,a]=t.useState(null),u=t.useCallback(d=>{a(d)},[]),g=t.useCallback(()=>{a(null)},[]);return{dialog:i?t.createElement(n,{openParams:i,close:g}):null,open:u}}}const X={title:"Hooks/createDialog",component:p},h=p(({openParams:n,close:r})=>{const i=()=>{n.onConfirm(),r()};return o.jsxs(f,{open:!0,onClose:r,children:[o.jsx(C,{children:"Confirm Action"}),o.jsx(D,{dividers:!1,children:n.message}),o.jsxs(x,{children:[o.jsx(m,{onClick:r,children:"Cancel"}),o.jsx(m,{variant:"contained",color:"secondary",onClick:i,children:"Confirm"})]})]})}),e=()=>{const n=h();return o.jsxs(o.Fragment,{children:[o.jsx(m,{onClick:()=>{n.open({message:"Are you sure you want to proceed with this action?",onConfirm:()=>{console.log("Action confirmed!")}})},children:"Open Confirm Dialog"}),n.dialog]})};var s,l,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  const confirmDialog = useConfirmDialog();
+  return <>
+      <Button onClick={() => {
+      confirmDialog.open({
+        message: 'Are you sure you want to proceed with this action?',
+        onConfirm: () => {
+          console.log('Action confirmed!');
+        }
+      });
+    }}>
+        Open Confirm Dialog
+      </Button>
+      {confirmDialog.dialog}
+    </>;
+}`,...(c=(l=e.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const Y=["ConfirmDialogExample"];export{e as ConfirmDialogExample,Y as __namedExportsOrder,X as default};
---
+++ assets/use_controllable-DL6EUlYr.js
@@ -0,0 +1,32 @@
+import{j as n}from"./jsx-runtime-xF634gn_.js";import{useMDXComponents as r}from"./index-o2KxC7bF.js";import{M as a,C as s}from"./index-4w__b9qQ.js";import{DemoExample as l}from"./use_controllable.stories-Dr4fXFDb.js";import"./index-C-7etoUd.js";import"./iframe-bUFeBjSn.js";import"./index-dzNl0v_V.js";import"./index-DWe4bgYs.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./use_controllable-DXs9Mygf.js";function o(t){const e={code:"code",h2:"h2",p:"p",pre:"pre",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Hooks/useControllableState"}),`
+`,n.jsx(e.h2,{id:"overview",children:"Overview"}),`
+`,n.jsx(e.p,{children:"React hook that allows any component handle controlled and uncontrolled modes, and provide control over its internal state."}),`
+`,n.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,n.jsx(e.pre,{children:n.jsx(e.code,{className:"language-jsx",children:`import { useControllableState } from '@peculiar/react-components';
+
+export const Demo = () => {
+  const [value, setValue] = useControllableState({ defaultValue: 40 });
+
+  return (
+    <div>
+      <button
+        onClick={() => setValue(value + 1)}
+        type="button"
+      >
+        +
+      </button>
+      <span>
+        {value}
+      </span>
+      <button
+        onClick={() => setValue(value - 1)}
+        type="button"
+      >
+        -
+      </button>
+    </div>
+  );
+};
+`})}),`
+`,n.jsx(e.h2,{id:"demo",children:"Demo"}),`
+`,n.jsx(s,{of:l})]})}function C(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{C as default};
