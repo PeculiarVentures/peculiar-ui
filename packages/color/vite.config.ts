3817a71c31a6a5cd9bbf84c4c2c210473a23482c
Donskov Donskov <donskovdmitriyv@gmail.com>
2026-01-19 13:32:56 +0200

    chore(deps): update vitest and related dependencies to version 4.0.17

--- packages/color/vite.config.ts
+++ packages/color/vite.config.ts
@@ -1,9 +1,10 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     globals: true, // allows using describe/test/expect without import
+    environment: 'node',
   },
 });
