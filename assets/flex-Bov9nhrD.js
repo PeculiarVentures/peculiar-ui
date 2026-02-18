6896894cb55758090bcf4b6304b8a3ee3f6483a6
donskov <donskov@users.noreply.github.com>
2026-02-18 11:39:32 +0000

    deploy: b0fb3c20e8ee8086adef1c544126bbf24dbe1ac6

---
+++ assets/flex-Bov9nhrD.js
@@ -0,0 +1,15 @@
+import{j as t}from"./jsx-runtime-xF634gn_.js";import{useMDXComponents as a}from"./index-o2KxC7bF.js";import{M as r,C as n,A as i}from"./index-B2aM2jSA.js";import{S as l,A as h,V as m,C as c}from"./flex.stories-BHV7S6TG.js";import{F as x,a as d}from"./flex_container-Cn7GkWK3.js";import"./index-C-7etoUd.js";import"./iframe-CSeEwzdF.js";import"./index-dzNl0v_V.js";import"./index-DWe4bgYs.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./box-BBNYSgcD.js";import"./emotion-styled.browser.esm-CnX2NPaZ.js";import"./emotion-element-f0de968e.browser.esm-DLlpVLeB.js";function s(o){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",...a(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:l}),`
+`,t.jsx(e.h1,{id:"flex",children:"Flex"}),`
+`,t.jsx(e.h2,{id:"auto-layout",children:"Auto-layout"}),`
+`,t.jsx(e.p,{children:"The auto-layout feature gives equal space to all items present. When you set the width of one item, the others will automatically resize to match it."}),`
+`,t.jsx(n,{of:h}),`
+`,t.jsx(e.h2,{id:"variable-width-content",children:"Variable width content"}),`
+`,t.jsxs(e.p,{children:["When a breakpoint's value is given as ",t.jsx(e.code,{children:"auto"}),", then a column's size will automatically adjust to match the width of its content. The demo below shows how this works:"]}),`
+`,t.jsx(n,{of:m}),`
+`,t.jsx(e.h2,{id:"column",children:"Column"}),`
+`,t.jsx(n,{of:c}),`
+`,t.jsx(e.h2,{id:"component-api",children:"Component API"}),`
+`,t.jsx(e.h3,{id:"flexcontainer",children:"FlexContainer"}),`
+`,t.jsx(i,{of:x}),`
+`,t.jsx(e.h3,{id:"flex-1",children:"Flex"}),`
+`,t.jsx(i,{of:d})]})}function D(o={}){const{wrapper:e}={...a(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(s,{...o})}):s(o)}export{D as default};
