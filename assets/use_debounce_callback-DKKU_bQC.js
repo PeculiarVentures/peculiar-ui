e108c9b632649ff47a7699c670b406942a6f71ce
donskov <donskov@users.noreply.github.com>
2026-02-18 11:34:22 +0000

    deploy: 89ad9fa47dff559b77e776c3228d8af2dfb53ef8

---
+++ assets/use_debounce_callback-DKKU_bQC.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-xF634gn_.js";import{useMDXComponents as s}from"./index-o2KxC7bF.js";import{M as a,C as r}from"./index-BWlLUkL2.js";import{DemoExample as c}from"./use_debounce_callback.stories-DM-dSggO.js";import"./index-C-7etoUd.js";import"./iframe-Br5xYDn4.js";import"./index-dzNl0v_V.js";import"./index-DWe4bgYs.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";function o(t){const n={code:"code",h2:"h2",p:"p",pre:"pre",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Hooks/useDebounceCallback"}),`
+`,e.jsx(n.h2,{id:"overview",children:"Overview"}),`
+`,e.jsxs(n.p,{children:["React hook that makes passed function debounced, otherwise acts like ",e.jsx(n.code,{children:"useCallback"}),"."]}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`import { useDebounceCallback } from '@peculiar/react-components';
+
+export const Demo = () => {
+  const [value, setValue] = React.useState('');
+  const setValueDebounced = useDebounceCallback(setValue, 300);
+
+  return (
+    <>
+      <p>
+        Below state will update 300ms after last change.
+      </p>
+      <p>
+        The input&apos;s value is:
+        {' '}
+        {value}
+      </p>
+      <input
+        type="text"
+        onChange={(event) => {
+          setValueDebounced(event.target.value);
+        }}
+      />
+    </>
+  );
+};
+`})}),`
+`,e.jsx(n.h2,{id:"demo",children:"Demo"}),`
+`,e.jsx(r,{of:c})]})}function v(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{v as default};
