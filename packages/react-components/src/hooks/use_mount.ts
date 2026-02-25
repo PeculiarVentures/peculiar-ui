a9b89eaf71be890aae6b184478536bc36f9df7e6
Donskov Donskov <donskovdmitriyv@gmail.com>
2026-02-25 14:29:55 +0200

    chore(react-components): replace useEnhancedEffect with React.useEffect for useMount

--- packages/react-components/src/hooks/use_mount.ts
+++ packages/react-components/src/hooks/use_mount.ts
@@ -1,9 +1,10 @@
-import { useEnhancedEffect } from './use_enhanced_effect';
+import React from 'react';
 
 /**
  * React hook that runs a callback only once when the component mounts.
  * @param callback The callback function to run on mount.
  */
 export function useMount(callback: () => void) {
-  useEnhancedEffect(callback, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  React.useEffect(callback, []);
 }
