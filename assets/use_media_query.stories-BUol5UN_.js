99631b1fbe4a16960c4d5a28b125155f7f222a52
donskov <donskov@users.noreply.github.com>
2026-03-11 09:33:26 +0000

    deploy: fa741685d80b3fbc485f4d867a5ce3f1351d16f2

--- assets/use_media_query.stories-BUol5UN_.js
+++
@@ -1,8 +0,0 @@
-import{j as u}from"./jsx-runtime-xF634gn_.js";import{e as p}from"./index-C-7etoUd.js";import{I as h,u as f}from"./use_enhanced_effect-BDfR00fA.js";function c(e,n=!1){const[d,m]=p.useState(()=>n!==void 0?n:h?!1:window.matchMedia(e).matches);return f(()=>{const t=window.matchMedia(e),r=()=>{m(t.matches)};return r(),t.addListener(r),()=>{t.removeListener(r)}},[e]),d}const M={title:"Hooks/useMediaQuery",component:c},s=()=>{const e=c("(min-width: 480px)");return u.jsxs("div",{children:["Screen is wide:"," ",e?"Yes":"No"]})};var i,o,a;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
-  const isWide = useMediaQuery('(min-width: 480px)');
-  return <div>
-      Screen is wide:
-      {' '}
-      {isWide ? 'Yes' : 'No'}
-    </div>;
-}`,...(a=(o=s.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["DemoExample"];export{s as DemoExample,l as __namedExportsOrder,M as default};
