f9b38005a6e75c1faa58d57e1a34abd6451539e0
donskov <donskov@users.noreply.github.com>
2026-03-27 08:20:11 +0000

    deploy: 7856acc06237060c2e1a3a67c37a37c747302000

---
+++ assets/create_dialog.stories-CiJ6Jpnz.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-xF634gn_.js";import{e as t}from"./index-C-7etoUd.js";import{B as a}from"./button-CHdIlyf5.js";import{D as f,a as C,b as D,c as x}from"./dialog_actions-CkSoL_nT.js";import"./emotion-styled.browser.esm-CyF2Egzp.js";import"./emotion-element-f0de968e.browser.esm-DLlpVLeB.js";import"./button_base-BEZzlAAv.js";import"./typography-65OyWQjn.js";import"./fade-BDSdpjve.js";import"./use_merged_ref-ClS6-mGQ.js";import"./Transition-D6I_MtWh.js";import"./index-DmZMPOxo.js";import"./index-BKjiqKB3.js";import"./circular_progress-B43_W6sf.js";import"./emotion-react.browser.esm-vU_FEsrj.js";import"./modal-CiZwoNyH.js";import"./portal-DuTPpHT8.js";import"./use_enhanced_effect-BDfR00fA.js";import"./box-CGVHNkbz.js";import"./collapse-cIkUIiUc.js";import"./alert-4qnj8TrD.js";import"./icon_button-C1THNxxI.js";import"./tooltip-BBxUZB1a.js";import"./use_controllable-DXs9Mygf.js";import"./popper-CBNtLXW5.js";import"./close_icon-DLJ9YGil.js";import"./flex_container-BymB3eux.js";import"./close_circle_icon-Nt97DkBN.js";import"./warning_icon-D6xCuvw-.js";function p(n){return function(){const[i,m]=t.useState(null),u=t.useCallback(d=>{m(d)},[]),g=t.useCallback(()=>{m(null)},[]);return{dialog:i?t.createElement(n,{openParams:i,close:g}):null,open:u}}}const W={title:"Hooks/createDialog",component:p},h=p(({openParams:n,close:r})=>{const i=()=>{n.onConfirm(),r()};return o.jsxs(f,{open:!0,onClose:r,children:[o.jsx(C,{children:"Confirm Action"}),o.jsx(D,{dividers:!1,children:n.message}),o.jsxs(x,{children:[o.jsx(a,{onClick:r,children:"Cancel"}),o.jsx(a,{variant:"contained",color:"secondary",onClick:i,children:"Confirm"})]})]})}),e=()=>{const n=h();return o.jsxs(o.Fragment,{children:[o.jsx(a,{onClick:()=>{n.open({message:"Are you sure you want to proceed with this action?",onConfirm:()=>{console.log("Action confirmed!")}})},children:"Open Confirm Dialog"}),n.dialog]})};var s,l,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  const confirmDialog = useConfirmDialog();
+  return <>
+      <Button onClick={() => {
+      confirmDialog.open({
+        message: 'Are you sure you want to proceed with this action?',
+        onConfirm: () => {
+          console.log('Action confirmed!');
+        }
+      });
+    }}>
+        Open Confirm Dialog
+      </Button>
+      {confirmDialog.dialog}
+    </>;
+}`,...(c=(l=e.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const X=["ConfirmDialogExample"];export{e as ConfirmDialogExample,X as __namedExportsOrder,W as default};
